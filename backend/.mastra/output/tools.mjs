3503f26f93a788fa4aa8910b3ae71c53cab29a4a
JojoDuke <jojoamankwa@gmail.com>
2026-02-21 16:52:42 +0000

    tsw

--- backend/.mastra/output/tools.mjs
+++
@@ -1,6 +0,0 @@
-import * as tool0 from './tools/28a677b9-843d-497e-b4a2-65ab0dd1cdbe.mjs';
-import * as tool1 from './tools/9aa44de6-05e1-43ec-ab7d-875c3ca75188.mjs';
-import * as tool2 from './tools/604564d1-a958-4972-aa86-eee51ede5a33.mjs';
-import * as tool3 from './tools/eb677471-603a-444e-9bb0-979e4b5104b0.mjs';
-
-                export const tools = [tool0, tool1, tool2, tool3]
